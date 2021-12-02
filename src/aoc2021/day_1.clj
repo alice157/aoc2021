f557fef3de520e7e0f2003b0dfbaa23f45e49e72
Alice Burns <aliceburns157@gmail.com>
2021-12-01 21:18:50 -0800

    day 1 part 2 solution

--- src/aoc2021/day_1.clj
+++ src/aoc2021/day_1.clj
@@ -7,6 +7,12 @@
 (defonce part-1-input (slurp "resources/day1.1.txt"))
 
 
+(def part-1-numbers
+  (->> part-1-input
+       (str/split-lines)
+       (map edn/read-string)))
+
+
 (defn increments
   [xs]
   (first (reduce (fn [[incs prev] n]
@@ -16,8 +22,25 @@
                  (rest xs))))
 
 
-(def part-1
-  (->> part-1-input
-       (str/split-lines)
-       (map edn/read-string)
+(def part-1 (increments part-1-numbers))
+
+
+(defn sliding-windows
+  ([ns window-size]
+   (sliding-windows (drop (dec window-size) ns)
+                    window-size
+                    (into (conj clojure.lang.PersistentQueue/EMPTY 0)
+                          (take (dec window-size) ns))))
+  ([ns window-size window]
+   (when-let [n (first ns)]
+     (let [window' (-> window
+                       (conj n)
+                       pop)]
+       (cons (seq window')
+             (lazy-seq (sliding-windows (rest ns) window-size window')))))))
+
+
+(def part-2
+  (->> (sliding-windows part-1-numbers 3)
+       (map #(apply + %))
        (increments)))
